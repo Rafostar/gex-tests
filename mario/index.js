572484ddec92c217a086a97687d0a0e9b939e82c
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-08-14 23:38:56 +0200

    Add hidden easter egg to Mario

--- mario/index.js
+++ mario/index.js
@@ -1,36 +1,33 @@
 const { Ink } = imports.ink;
 
-const W = Ink.TextColor.WHITE;
-const R = Ink.TextColor.RED;
-const S = Ink.TextColor.LIGHT_SALMON;
-const Y = Ink.TextColor.LIGHT_ORANGE;
-const H = Ink.TextColor.BROWN;
-const B = Ink.TextColor.BLUE;
-const K = Ink.TextColor.BLACK;
+const D = 'DEFAULT';
+const R = 'RED';
+const S = 'LIGHT_SALMON';
+const O = 'LIGHT_ORANGE';
+const H = 'BROWN';
+const B = 'BLUE';
+const K = 'BLACK';
 
 const MARIO = [
-    [[16,W]],
-    [[5,W],[5,R],[6,W]],
-    [[4,W],[9,R],[3,W]],
-    [[4,W],[3,H],[2,S],[1,K],[1,S],[5,W]],
-    [[3,W],[1,H],[1,S],[1,H],[3,S],[1,K],[3,S],[3,W]],
-    [[3,W],[1,H],[1,S],[2,H],[3,S],[1,K],[3,S],[2,W]],
-    [[4,W],[1,H],[4,S],[4,K],[3,W]],
-    [[5,W],[6,S],[5,W]],
-    [[4,W],[2,R],[1,B],[2,R],[1,B],[2,R],[4,W]],
-    [[3,W],[3,R],[1,B],[2,R],[1,B],[3,R],[3,W]],
-    [[2,W],[4,R],[4,B],[4,R],[2,W]],
-    [[2,W],[2,S],[1,R],[1,B],[1,Y],[2,B],[1,Y],[1,B],[1,R],[2,S],[2,W]],
-    [[2,W],[3,S],[6,B],[3,S],[2,W]],
-    [[2,W],[2,S],[8,B],[2,S],[2,W]],
-    [[4,W],[3,B],[2,W],[3,B],[4,W]],
-    [[3,W],[3,H],[4,W],[3,H],[3,W]],
-    [[2,W],[4,H],[4,W],[4,H],[2,W]]
+    [[5,D],[5,R],[6,D]],
+    [[4,D],[9,R],[3,D]],
+    [[4,D],[3,H],[2,S],[1,K],[1,S],[5,D]],
+    [[3,D],[1,H],[1,S],[1,H],[3,S],[1,K],[3,S],[3,D]],
+    [[3,D],[1,H],[1,S],[2,H],[3,S],[1,K],[3,S],[2,D]],
+    [[4,D],[1,H],[4,S],[4,K],[3,D]],
+    [[5,D],[6,S],[5,D]],
+    [[4,D],[2,R],[1,B],[2,R],[1,B],[2,R],[4,D]],
+    [[3,D],[3,R],[1,B],[2,R],[1,B],[3,R],[3,D]],
+    [[2,D],[4,R],[4,B],[4,R],[2,D]],
+    [[2,D],[2,S],[1,R],[1,B],[1,O],[2,B],[1,O],[1,B],[1,R],[2,S],[2,D]],
+    [[2,D],[3,S],[6,B],[3,S],[2,D]],
+    [[2,D],[2,S],[8,B],[2,S],[2,D]],
+    [[4,D],[3,B],[2,D],[3,B],[4,D]],
+    [[3,D],[3,H],[4,D],[3,H],[3,D]],
+    [[2,D],[4,H],[4,D],[4,H],[2,D]],
 ];
 
-let printer = new Ink.Printer({
-    font: Ink.TextFont.REVERSE
-});
+let printer = new Ink.Printer();
 
 for(let line of MARIO) {
     let str = '';
@@ -40,7 +37,9 @@ for(let line of MARIO) {
         while(count--)
             part += ' ';
 
-        printer.color = el[1];
+        printer.color = (el[1] === R)
+            ? Ink.Color.GREEN : Ink.Color[el[1]];
+        printer.background = Ink.Color[el[1]];
         part = printer.getPainted(part);
         str += part;
     }
